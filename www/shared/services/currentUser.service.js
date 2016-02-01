a4edff2ffe4260c8cffea793c5c9cd40bc272481
Melissa Wilcox <melissa@emdubb.co>
2016-02-01 11:43:33 -0800

    add methods to update and return current user

---
+++ www/shared/services/currentUser.service.js
@@ -0,0 +1,30 @@
+(function(){
+  "use strict"
+
+  angular
+    .module('tsunamiApp')
+    .factory('currentUser', currentUser);
+
+  currentUser.$inject = ["$log"];
+
+  function currentUser($log){
+    return {
+      user: user,
+      createUser: createUser,
+      addMap: addMap
+    }
+
+    var user = {}
+
+    function createUser(dbUser){
+      user = dbUser
+      return user
+    }
+
+    function addMap(dbMap){
+      $log.log("adding map: ", dbMap)
+      user.maps.push(dbMap)
+    }
+  }
+
+})()
