117060e6b78d1c537f454c3e4951e4f9325c62af
Melissa Wilcox <melissa@emdubb.co>
2016-02-03 00:39:56 -0800

    add methods to display modals add addMap method from the preview controller

--- www/components/maps/maps.controller.js
+++ www/components/maps/maps.controller.js
@@ -5,9 +5,9 @@
     .module("tsunamiApp")
     .controller("MapsController", MapsController);
 
-  MapsController.$inject = ["$state", "$log", "$http", "localStorageService", "urlFactory", "currentUser"]
+  MapsController.$inject = ["$state", "$log", "$http", "$scope", "$ionicModal", "$ionicPopup", "$ionicScrollDelegate", "localStorageService", "urlFactory", "currentUser"]
 
-  function MapsController($state, $log, $http, localStorageService, urlFactory, currentUser){
+  function MapsController($state, $log, $http, $scope, $ionicModal, $ionicPopup, $ionicScrollDelegate, localStorageService, urlFactory, currentUser){
 
     var vm = this;
     var url = urlFactory
@@ -15,6 +15,7 @@
     vm.previewMap = previewMap
     vm.showMap = showMap
     vm.user = currentUser.user
+    vm.addMap = addMap
 
     $http({
       method: 'GET',
@@ -54,6 +55,92 @@
       localStorageService.saveData("map", id)
       $state.go("tab.map-detail", {"id": id})
     }
+
+    // ====================
+    // Show Maps
+    // ====================
+
+    $ionicModal.fromTemplateUrl('components/maps/mapDetailModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.detailModalodal = modal;
+      });
+      $scope.openDetailModal = function(map) {
+        vm.mapDetail = map
+        $scope.detailModalodal.show();
+      };
+      $scope.closeDetailModal = function() {
+        $scope.detailModalodal.hide();
+      };
+      //Cleanup the modal when we're done with it!
+      $scope.$on('$destroy', function() {
+        $scope.detailModalodal.remove();
+      });
+      // Execute action on hide modal
+      $scope.$on('detailModalodal.hidden', function() {
+        // Execute action
+      });
+      // Execute action on remove modal
+      $scope.$on('detailModalodal.removed', function() {
+        // Execute action
+      });
+
+    // ====================
+    // Preview Maps
+    // ====================
+
+    $ionicModal.fromTemplateUrl('components/maps/mapPreviewModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.previewModal = modal;
+      });
+      $scope.openPreviewModal = function() {
+        $scope.previewModal.show();
+      };
+      $scope.closePreviewModal = function() {
+        $scope.previewModal.hide();
+      };
+      //Cleanup the modal when we're done with it!
+      $scope.$on('$destroy', function() {
+        $scope.previewModal.remove();
+      });
+      // Execute action on hide modal
+      $scope.$on('previewModal.hidden', function() {
+        // Execute action
+      });
+      // Execute action on remove modal
+      $scope.$on('previewModal.removed', function() {
+        // Execute action
+      });
+
+      function addMap() {
+        $log.log("add map!")
+        $http({
+          method: 'PATCH',
+          url: url + '/users/' + localStorageService.loadData("user") + '?map_id=' + vm.previewMap.id + '&add=true',
+          contentType: 'application/json',
+          headers: {
+            'Authorization': localStorageService.loadData('token')
+          }
+        }).then(function successCallback(response){
+          var alertPopup = $ionicPopup.alert({
+            title: 'Map successfully saved!',
+          });
+          currentUser.addMap(vm.previewMap)
+          alertPopup.then(function(res) {
+            // $state.go('tab.maps')
+            vm.showme = !vm.showme
+            $scope.closePreviewModal()
+            // $ionicScrollDelegate.scrollTop();
+          });
+        }, function errorCallback(response){
+          var alertPopup = $ionicPopup.alert({
+            title: 'Uh oh, something went wrong! Please try again.',
+          });
+        });
+       };
 
   }
 
