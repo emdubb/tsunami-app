a1b5f27c442a4a9ca50dda2d7f172896b78fcada
Melissa Wilcox <melissa@emdubb.co>
2016-02-03 01:22:26 -0800

    add scrolling on double tap

--- www/components/maps/maps.controller.js
+++ www/components/maps/maps.controller.js
@@ -11,11 +11,13 @@
 
     var vm = this;
     var url = urlFactory
+    var zoom = 0
     vm.counties = ["San Diego"]
     vm.previewMap = previewMap
     vm.showMap = showMap
     vm.user = currentUser.user
     vm.addMap = addMap
+    vm.onDoubleTap = onDoubleTap
 
     $http({
       method: 'GET',
@@ -61,86 +63,104 @@
     // ====================
 
     $ionicModal.fromTemplateUrl('components/maps/mapDetailModal.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-        $scope.detailModalodal = modal;
-      });
-      $scope.openDetailModal = function(map) {
-        vm.mapDetail = map
-        $scope.detailModalodal.show();
-      };
-      $scope.closeDetailModal = function() {
-        $scope.detailModalodal.hide();
-      };
-      //Cleanup the modal when we're done with it!
-      $scope.$on('$destroy', function() {
-        $scope.detailModalodal.remove();
-      });
-      // Execute action on hide modal
-      $scope.$on('detailModalodal.hidden', function() {
-        // Execute action
-      });
-      // Execute action on remove modal
-      $scope.$on('detailModalodal.removed', function() {
-        // Execute action
-      });
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.detailModalodal = modal;
+    });
+    $scope.openDetailModal = function(map) {
+      vm.mapDetail = map
+      $scope.detailModalodal.show();
+    };
+    $scope.closeDetailModal = function() {
+      $ionicScrollDelegate.$getByHandle('mapZoom').zoomTo(1, false)
+      $scope.detailModalodal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.detailModalodal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('detailModalodal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('detailModalodal.removed', function() {
+      // Execute action
+    });
 
     // ====================
     // Preview Maps
     // ====================
 
     $ionicModal.fromTemplateUrl('components/maps/mapPreviewModal.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-        $scope.previewModal = modal;
-      });
-      $scope.openPreviewModal = function() {
-        $scope.previewModal.show();
-      };
-      $scope.closePreviewModal = function() {
-        $scope.previewModal.hide();
-      };
-      //Cleanup the modal when we're done with it!
-      $scope.$on('$destroy', function() {
-        $scope.previewModal.remove();
-      });
-      // Execute action on hide modal
-      $scope.$on('previewModal.hidden', function() {
-        // Execute action
-      });
-      // Execute action on remove modal
-      $scope.$on('previewModal.removed', function() {
-        // Execute action
-      });
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.previewModal = modal;
+    });
+    $scope.openPreviewModal = function() {
+      $scope.previewModal.show();
+    };
+    $scope.closePreviewModal = function() {
+      $scope.previewModal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.previewModal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('previewModal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('previewModal.removed', function() {
+      // Execute action
+    });
 
-      function addMap() {
-        $log.log("add map!")
-        $http({
-          method: 'PATCH',
-          url: url + '/users/' + localStorageService.loadData("user") + '?map_id=' + vm.previewMap.id + '&add=true',
-          contentType: 'application/json',
-          headers: {
-            'Authorization': localStorageService.loadData('token')
-          }
-        }).then(function successCallback(response){
-          var alertPopup = $ionicPopup.alert({
-            title: 'Map successfully saved!',
-          });
-          currentUser.addMap(vm.previewMap)
-          alertPopup.then(function(res) {
-            // $state.go('tab.maps')
-            vm.showme = !vm.showme
-            $scope.closePreviewModal()
-            // $ionicScrollDelegate.scrollTop();
-          });
-        }, function errorCallback(response){
-          var alertPopup = $ionicPopup.alert({
-            title: 'Uh oh, something went wrong! Please try again.',
-          });
+    function addMap() {
+      $log.log("add map!")
+      $http({
+        method: 'PATCH',
+        url: url + '/users/' + localStorageService.loadData("user") + '?map_id=' + vm.previewMap.id + '&add=true',
+        contentType: 'application/json',
+        headers: {
+          'Authorization': localStorageService.loadData('token')
+        }
+      }).then(function successCallback(response){
+        var alertPopup = $ionicPopup.alert({
+          title: 'Map successfully saved!',
         });
-       };
+        currentUser.addMap(vm.previewMap)
+        alertPopup.then(function(res) {
+          // $state.go('tab.maps')
+          vm.showme = !vm.showme
+          $scope.closePreviewModal()
+          // $ionicScrollDelegate.scrollTop();
+        });
+      }, function errorCallback(response){
+        var alertPopup = $ionicPopup.alert({
+          title: 'Uh oh, something went wrong! Please try again.',
+        });
+      });
+    };
+
+    // ====================
+    // Image zooming
+    // ====================
+
+    function onDoubleTap(){
+      if (zoom === 0) {
+        zoom++
+        $ionicScrollDelegate.$getByHandle('mapZoom').zoomTo(2, true)
+      } else {
+        zoom--
+        $ionicScrollDelegate.$getByHandle('mapZoom').zoomTo(1, true)
+      }
+    }
+
+
+
 
   }
 
